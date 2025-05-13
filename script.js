8a6930548ba3aa957169f6c1cc4f951ac8daf337
SHAHWAIZ <shahwaiztiger12@gmail.com>
2025-05-13 19:18:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const headers = document.querySelectorAll('.accordion-header');
+
+headers.forEach(header => {
+  header.addEventListener('click', () => {
+    const content = header.nextElementSibling;
+    header.classList.toggle('active');
+    content.classList.toggle('show');
+  });
+});
